00a9a96738f0112bc7285c6dc02cee923ebafbe8
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-10 16:28:51 -0800

    cors

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ var favoriteTeamsRouter=require('./routes/favorite_teams')
 var favoriteAthletesRouter=require('./routes/favorite_athletes')
 var favoriteSportsRouter=require('./routes/favorite_sports')
 var app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
