9e19d6a0b374b6f811d74876ab9fd3a10b18f1b6
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-26 10:44:26 -0800

    postgres

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 require('dotenv').config()
 
 var express = require('express');
@@ -22,7 +17,12 @@ var favoriteAthletesRouter=require('./routes/favorite_athletes')
 var favoriteSportsRouter=require('./routes/favorite_sports')
 var rostersRouter=require('./routes/rosters');
 var app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
