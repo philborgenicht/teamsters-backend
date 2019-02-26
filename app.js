290b74b1bb101c67d5c93592280a3c5cf5523868
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-26 13:44:49 -0800

    cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var athletesRouter= require('./routes/athletes');
@@ -16,19 +16,23 @@ var favoriteTeamsRouter=require('./routes/favorite_teams')
 var favoriteAthletesRouter=require('./routes/favorite_athletes')
 var favoriteSportsRouter=require('./routes/favorite_sports')
 var rostersRouter=require('./routes/rosters');
+
+var cors = require('cors');
 var app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
