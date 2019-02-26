b7c2256d39a336367d1b40f288f0b8ee4f40cc9e
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-26 10:56:54 -0800

    cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({'https://localhost:3000'}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
