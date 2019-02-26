6e9a4db521992b318453a2d5685a81de9bb3266a
Phillip Borgenicht <philborgenicht@gmail.com>
2019-02-26 13:55:51 -0800

    heroku

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://ojvjbboiytxlcb:22e581d5c860cecf48b16cd2d5698ec8a1a02f361afade1484121f4d740aa27a@ec2-54-225-237-84.compute-1.amazonaws.com:5432/d1jcd6c9ic76lv'
+    connection: 'postgresql://localhost:5432/walterwhite'
   },
   test: {
     client: 'pg',
-    connection: 'postgres://ojvjbboiytxlcb:22e581d5c860cecf48b16cd2d5698ec8a1a02f361afade1484121f4d740aa27a@ec2-54-225-237-84.compute-1.amazonaws.com:5432/d1jcd6c9ic76lv'
+    connection: 'postgresql://localhost:5432/walterwhite'
   },
   production: {
     client: 'pg',
